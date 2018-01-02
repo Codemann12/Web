7d0d181bb02e78d1a2c9415137c8054996aee621
Codemann12 <fbcedric@yahoo.fr>
2018-01-02 16:34:50 +0100

    Add some Javascript.

--- MyWeb/Index.php
+++ MyWeb/Index.php
@@ -50,6 +50,37 @@
              </div>
       </div>
 
+<div class="Slide">
+   <img class="mySlides" src="Images/1.jpg" style="width:100%">
+   <img class="mySlides" src="Images/2.jpg" style="width:100%">
+   <img class="mySlides" src="Images/3.jpg" style="width:100%">
+  <button class="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+  <button class="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
+</div>
+
+<script type="text/javascript">
+  
+  var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+</script>
+
+
+
     <div id="blockTitle">
       <h1>Style of the day </h1>
     </div>
